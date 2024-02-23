0df8dfce99b938a2be24a9dd1a2d22a24a2eed09
JeonYooDeok <jyd910215@gmail.com>
2024-02-23 14:46:13 +0900

    투두함수 전역사용을 위한 구조변경 전 백업표시

--- src/Hooks/useInput.jsx
+++ src/Hooks/useInput.jsx
@@ -3,10 +3,10 @@ import { InputContext } from "../Contexts/InputContext";
 
 //useModal 커스텀 훅을 정의
 export const useInput = () => {
-  const context = useContext(InputContext); //모달 컨텍스트를 가져오기
+  const context = useContext(InputContext); //인풋 컨텍스트를 가져오기
   if (!context) {
-    //모달 컨텍스트가 없으면 오류를 발생
-    throw new Error("useModal은 모달 프로바이더 내에서 사용해야 합니다.");
+    //인풋 컨텍스트가 없으면 오류를 발생
+    throw new Error("useInput은 인풋 프로바이더 내에서 사용해야 합니다.");
   }
-  return context; //모달 컨텍스트를 반환
+  return context; //인풋 컨텍스트를 반환
 };
