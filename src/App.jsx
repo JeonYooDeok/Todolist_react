cd8fd463309e1089a0c12b6d5a2e9c23c17e8a64
JeonYooDeok <jyd910215@gmail.com>
2024-02-28 19:58:45 +0900

    헤더 및 라우터추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import React, { useEffect } from "react";
-import Todolist from "./Components/Todolist";
-import Todolist_2 from "./Components/Todolist_2";
-import Todolist_3 from "./Components/Todolist_3";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
 import Modal from "./Components/Modal";
 import { ModalProvider } from "./Contexts/ModalContext";
 import { TodoProvider } from "./Contexts/TodoContext";
+import Todolist_3 from "./Components/Todolist_3";
 
 function App() {
   return (
     <ModalProvider>
       <TodoProvider>
-        <Todolist_3 />
-        <Modal />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/Todolist_3"
+              element={<Todolist_3 />}
+            />
+          </Routes>
+          <Modal />
+        </BrowserRouter>
       </TodoProvider>
     </ModalProvider>
   );
