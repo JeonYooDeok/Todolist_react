0c3f7ece925349b0c4a50a569e4da679db9f1da0
JeonYooDeok <jyd910215@gmail.com>
2024-03-01 17:12:16 +0900

    파이어베이스 배포 테스트

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDauJ53d8MlGvSe3fHb5LEjTScw30Q45uY",
+  authDomain: "todolist-react-5cca0.firebaseapp.com",
+  projectId: "todolist-react-5cca0",
+  storageBucket: "todolist-react-5cca0.appspot.com",
+  messagingSenderId: "340973460810",
+  appId: "1:340973460810:web:6050b93d6682cd5217b5b3"
+};
+
+const app = initializeApp(firebaseConfig);
