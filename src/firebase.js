722875f2b0dd0d4b6c5560e9054ff890f0001e03
JeonYooDeok <jyd910215@gmail.com>
2024-03-01 20:42:31 +0900

    환경변수 세팅

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDauJ53d8MlGvSe3fHb5LEjTScw30Q45uY",
-  authDomain: "todolist-react-5cca0.firebaseapp.com",
-  projectId: "todolist-react-5cca0",
-  storageBucket: "todolist-react-5cca0.appspot.com",
-  messagingSenderId: "340973460810",
-  appId: "1:340973460810:web:6050b93d6682cd5217b5b3"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_API_KEY,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
