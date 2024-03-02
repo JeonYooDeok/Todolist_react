826eaea1e26be4072738cec056d04aea2886a327
JeonYooDeok <jyd910215@gmail.com>
2024-03-03 08:14:53 +0900

    로그아웃 구현

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useModal } from "../Hooks/useModal";
 import { Link } from "react-router-dom";
+import { authService } from "../firebase";
 
 function Header() {
   const { openModal } = useModal();
+  const logout = () => {
+    authService.signOut();
+  };
   return (
     <nav>
       <ul>
@@ -15,6 +19,7 @@ function Header() {
         </li>
         <button onClick={() => openModal("ModalLogin")}>로그인</button>
         <button onClick={() => openModal("ModalSignIn")}>회원가입</button>
+        <button onClick={logout}>로그아웃</button>
       </ul>
     </nav>
   );
